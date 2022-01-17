42732c7ae50bdd514ae184332d40d7c5df289f50
Evan Rauner <erauner@medallia.com>
2022-01-17 17:26:49 -0600

    update things 3 task's tag

--- lib/things3.py
+++ lib/things3.py
@@ -1,3 +1,4 @@
+from pyThings.tasks import UpdateTask
 
 import subprocess
 import json
@@ -8,3 +9,13 @@ def get_today_tasks():
                              stdin=subprocess.PIPE,
                              encoding='utf8')
     return json.loads(process.stdout)
+
+
+def update_all_things_tasks(auth_token, things3_today_tasks):
+    for item in things3_today_tasks:
+        update_task(auth_token, item["uuid"], add_tags=["sorted3"])
+    
+def update_task(auth_token, task_id, add_tags):
+        UpdateTask(auth_token=auth_token, task_id=task_id, add_tags=add_tags)
+
+    
