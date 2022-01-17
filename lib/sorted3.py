5fde0b11da208c955383c349d7c2ee4f493376b3
Evan Rauner <erauner@medallia.com>
2022-01-17 17:26:31 -0600

    add sorted task

--- lib/sorted3.py
+++ lib/sorted3.py
@@ -1,8 +1,20 @@
 
 from pySorted.tasks import AddTask
 
+from datetime import date
 
-def add_sorted_task(title, start_date, list_choice):
+
+def add_all_things_tasks(things_tasks_list):
+    """Adds all tasks from Sorted."""
+    
+    for task in things_tasks_list:
+        today_date = date.today().strftime('%Y-%m-%d')
+        add_sorted_task(task["title"],today_date,"things3","10","30",task["tags"])
+    
+    
+    
+
+def add_sorted_task(title, start_date, list_choice, earlyAlert, duration, tags):
     """Add a task to Sorted."""
-    task = AddTask(title,start_date,list_choice)
+    task = AddTask(title,start_date,list_choice, earlyAlert, duration, tags)
     return task.callback_url
