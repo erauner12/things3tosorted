edbb5cab737e8d59888143c2fcae381c649fd4f5
Evan Rauner <erauner@medallia.com>
2022-01-17 19:01:12 -0600

    Move task tag update to right after tag task in sorted3

--- lib/sorted3.py
+++ lib/sorted3.py
@@ -3,13 +3,24 @@ from pySorted.tasks import AddTask
 
 from datetime import date
 
+from lib.things3 import update_task_tag
+
+
 
 def add_all_things_tasks(things_tasks_list):
     """Adds all tasks from Sorted."""
     
+    today_date = date.today().strftime('%Y-%m-%d')
     for task in things_tasks_list:
-        today_date = date.today().strftime('%Y-%m-%d')
-        add_sorted_task(task["title"],today_date,"things3","10","30",task["tags"])
+        if "sorted3" not in task.get("tags","fallback"):
+            # TODO: You should be passing these as args so that you do not have to handle errors like this
+            try:
+                add_sorted_task(task["title"],today_date,"things3","10","30",task["tags"])
+                update_task_tag(task["uuid"], add_tags=["sorted3"])
+            except KeyError as e:    
+                add_sorted_task(task["title"],today_date,"things3","10","30",["None"])
+                update_task_tag(task["uuid"], add_tags=["sorted3"])
+                
     
     
     
--- lib/things3.py
+++ lib/things3.py
@@ -3,19 +3,16 @@ from pyThings.tasks import UpdateTask
 import subprocess
 import json
 
+auth_token = "zHtl26BeQnW5CuTDxgAfBw"
+
 def get_today_tasks():
     process = subprocess.run(args=["./bin/things-cli","--json", "today"],
                              stdout=subprocess.PIPE,
                              stdin=subprocess.PIPE,
                              encoding='utf8')
     return json.loads(process.stdout)
-
-
-def update_all_things_tasks(auth_token, things3_today_tasks):
-    for item in things3_today_tasks:
-        update_task(auth_token, item["uuid"], add_tags=["sorted3"])
     
-def update_task(auth_token, task_id, add_tags):
+def update_task_tag(task_id, add_tags):
         UpdateTask(auth_token=auth_token, task_id=task_id, add_tags=add_tags)
 
     
